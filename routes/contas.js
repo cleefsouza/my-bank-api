2da9e6ce0c50d5ad16acd14573ec89df7f5e807e
Cleef Souza <a.cleef.souza@gmail.com>
2020-06-19 07:55:55 -0300

    Requisição PUT inicial

--- routes/contas.js
+++ routes/contas.js
@@ -79,4 +79,68 @@ router.delete("/:id", (req, res) => {
     });
 });
 
+router.put("/", (req, res) => {
+    const body = req.body;
+
+    fs.readFile(file, "utf8", (err, data) => {
+        if (err) {
+            res.status(400).json({ mensagem: "Erro ao ler arquivo." });
+        }
+
+        let json = JSON.parse(data);
+
+        const index = json.contas.findIndex(
+            (item) => item.id === parseInt(body.id)
+        );
+
+        if (!index) {
+            res.status(404).json({ mensagem: "Conta não encontrada." });
+        }
+
+        json.contas[index] = body;
+
+        fs.writeFile(file, JSON.stringify(json), (err) => {
+            if (err) {
+                res.status(400).json({ mensagem: err.message });
+            }
+        });
+
+        res.json({ mensagem: "Conta atualizada com sucesso!" });
+    });
+});
+
+router.put("/deposito", (req, res) => {
+    const body = req.body;
+
+    fs.readFile(file, "utf8", (err, data) => {
+        if (err) {
+            res.status(400).json({ mensagem: "Erro ao ler arquivo." });
+        }
+
+        let json = JSON.parse(data);
+
+        const index = json.contas.findIndex(
+            (item) => item.id === parseInt(body.id)
+        );
+
+        if (!index) {
+            res.status(404).json({ mensagem: "Conta não encontrada." });
+        }
+
+        if (body.saldo <= 0) {
+            res.status(400).json({ mensagem: "Valor inválido para depósito." });
+        }
+
+        json.contas[index].saldo += body.saldo;
+
+        fs.writeFile(file, JSON.stringify(json), (err) => {
+            if (err) {
+                res.status(400).json({ mensagem: err.message });
+            }
+        });
+
+        res.json({ mensagem: "Saldo atualizado com sucesso!" });
+    });
+});
+
 module.exports = router;
